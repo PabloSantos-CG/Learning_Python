7fea37207bf77e89637e1ee5cd96108575eb0e77
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-15 11:04:50 -0300

    up

--- ex01/main.py
+++ ex01/main.py
@@ -1,1 +1,10 @@
-print("Hello World");
+# Abrir o arquivo no modo de escrita
+arquivo = open("meuarquivo.txt", "w")
+
+# Escrever no arquivo
+arquivo.write("Olá, mundo!")
+
+# Fechar o arquivo
+arquivo.close()
+
+print("Arquivo criado e escrito com sucesso!")
