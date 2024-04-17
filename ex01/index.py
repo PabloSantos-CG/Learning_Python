994453823d5251eb3bd0ab92c2b6a557d8a25055
PabloSantos-CG <comercial-pablo@outlook.com>
2024-04-17 18:01:53 -0300

    update

--- ex01/index.py
+++ ex01/index.py
@@ -6,22 +6,23 @@ def validate(cpf_param: str):
   
   return None
 
-def separator(cpf_param: str) -> list[int]:
+def separator(cpf_param: str, limit: int) -> list[int]:
   cpf_list = []
 
   for index, value in enumerate(cpf_param):
     cpf_list.append(int(value))
     
-    if (index == 8):
+    if (index == limit):
       return cpf_list
     
 
 def calculate(cpf_param: str):
   count = 10
   accumulator = 0
-  cpf_list = separator(cpf_param)
+  first_list = separator(cpf_param, 8)
+  # lasst_list = separator(cpf_param, 9)
 
-  for digit in enumerate(cpf_list):
+  for digit in enumerate(first_list):
     accumulator += digit[1] * count
     count -= 1
   
